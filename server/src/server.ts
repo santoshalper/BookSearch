43052aa895d706ece6b147a663845ea4b29fc605
Vijay <vijaynatarajan@protonmail.com>
2025-03-28 21:41:58 -0400

    test2

--- server/src/server.ts
+++ server/src/server.ts
@@ -28,10 +28,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static( '../../../client/dist'));
+    app.use(express.static( './client/dist'));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile('../../../client/dist/index.html');
+      res.sendFile('./client/dist/index.html');
     });
   }
 
